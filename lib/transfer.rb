d91d2ba6692fadd2e34cc0b11d305e45102beead
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-11-01 18:36:49 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,6 +2,9 @@ class Transfer
   attr_accessor :transfer, :status
 
   def initialize(sender, reciever, amount)
+    @sender = sender
+    @reciever = reciever
+    @amount = amount
     @status = "pending"
   end
 end
